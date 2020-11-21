da3c14d5bb1198fc2d33fb81963c0e1835fa7250
skrasek.michal <skrasek.michal@email.cz>
2020-11-22 00:13:24 +0100

    added special info for syntax error

--- src/ifj20.c
+++ src/ifj20.c
@@ -15,8 +15,18 @@ int main()
 
     int result = parse(&data);
     if (result != 0)
-        fprintf(stderr, "%d - line: %d\n", result, data.token.line);
-
+    {
+        if (result == ERR_SYNTAX)
+        {
+            if (data.token.type == TOKEN_KEYWORD)
+                fprintf(stderr, "syntax error: unexpected token keyword %d at line %d\n", data.token.attr.kw, data.token.line);
+            else
+                fprintf(stderr, "syntax error: unexpected token %d at line %d\n", data.token.type, data.token.line);
+            fprintf(stderr, "token sequence: %d %d\n", data.prev_token.type, data.token.type);
+        }
+        else
+            fprintf(stderr, "error %d - line: %d\n", result, data.token.line);
+    }
     dispose_data(&data);
     str_free(&s);
 
