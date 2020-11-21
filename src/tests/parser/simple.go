b6f672e60bdd923ec43d9b92846833127e5972ef
pkabelka <trins13@seznam.cz>
2020-11-21 20:41:40 +0100

    added test files

---
+++ src/tests/parser/simple.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	a := 1 /*dawdw*/
+	/* dwad
+
+		aaaaaaa
+
+	aaaa
+
+			aw*/
+	// daw
+
+	/// adwad
+
+	/// dawdada
+	// comment
+	_ = a // dawdad
+	return
+}
+
+//dwadaw
