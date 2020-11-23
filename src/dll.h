b650e018cd4441fb11bb1c2e6e057a5e10835d3d
MoudryDaniel <xmoudr01@stud.fit.vutbr.cz>
2020-11-23 18:10:20 +0100

    changes in function names, some return types and some parameter types

--- src/dll.h
+++ src/dll.h
@@ -12,19 +12,19 @@ typedef struct dll_t {
     dll_node_t *last;
 } dll_t;
 
-dll_t* dllInit(dll_t*);
-void joinLists(dll_t*, dll_t*);
+dll_t* dll_init(dll_t*);
+void join_lists(dll_t*, dll_t*);
 
-void* dllGet(dll_t*, int);
-void* dllGetFirst(dll_t*);
-void* dllGetLast(dll_t*);
+void* dll_get(dll_t*, int);
+void* dll_get_first(dll_t*);
+void* dll_get_last(dll_t*);
 
-void dllInsert(dll_t*, int, int);
-void dllInsertFirst(dll_t*, int);
-void dllInsertLast(dll_t*, int);
+bool dll_insert(dll_t*, int, void*);
+bool dll_insert_first(dll_t*, void*);
+bool dll_insert_last(dll_t*, void*);
 
-void dllDelete(dll_t*, int, void (deletePtr*)(dll_node_t*));
-void dllDeleteFirst(dll_t*, void (deletePtr*)(dll_node_t*));
-void dllDeleteLast(dll_t*, void (deletePtr*)(dll_node_t*));
+bool dll_delete(dll_t*, int, void (delete_ptr*)(void*));
+bool dll_delete_first(dll_t*, void (delete_ptr*)(void*));
+bool dll_delete_last(dll_t*, void (delete_ptr*)(void*));
 
-void dllDispose(dll_t*, void (deletePtr*)(dll_node_t*));
+bool dll_dispose(dll_t*, void (delete_ptr*)(void*));
