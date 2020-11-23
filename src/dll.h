df76f1e1f1b8ea794365daeb20afa6372df31633
MoudryDaniel <xmoudr01@stud.fit.vutbr.cz>
2020-11-23 16:48:59 +0100

    added pointers to free function, added join function, another minor changes

--- src/dll.h
+++ src/dll.h
@@ -1,20 +1,19 @@
-#define TRUE 1
-#define FALSE 0
+#include <stdbool.h>
 
 typedef struct dll_node_t {
-	void *data;
+    void *data;
     struct dll_node_t *prev;
     struct dll_node_t *next;
 } dll_node_t;
 
 typedef struct dll_t {
     int size;
-	dll_node_t *first;
+    dll_node_t *first;
     dll_node_t *last;
 } dll_t;
 
 dll_t* dllInit(dll_t*);
-dll_node_t* dllSearch(dll_t*, int);
+void joinLists(dll_t*, dll_t*);
 
 void* dllGet(dll_t*, int);
 void* dllGetFirst(dll_t*);
@@ -24,8 +23,8 @@ void dllInsert(dll_t*, int, int);
 void dllInsertFirst(dll_t*, int);
 void dllInsertLast(dll_t*, int);
 
-void dllDelete(dll_t*, int);
-void dllDeleteFirst(dll_t*);
-void dllDeleteLast(dll_t*);
+void dllDelete(dll_t*, int, void (deletePtr*)(dll_node_t*));
+void dllDeleteFirst(dll_t*, void (deletePtr*)(dll_node_t*));
+void dllDeleteLast(dll_t*, void (deletePtr*)(dll_node_t*));
 
-void dllDispose(dll_t*);
+void dllDispose(dll_t*, void (deletePtr*)(dll_node_t*));
