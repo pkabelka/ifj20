9d374ba900945374c0feafcd6bd596fbf6e5492a
MoudryDaniel <xmoudr01@stud.fit.vutbr.cz>
2020-11-29 18:32:45 +0100

    icgeneraotr.h with structure definitions and function prototype

---
+++ src/icgenerator.h
@@ -0,0 +1,37 @@
+#ifndef _ICGENERATOR_H
+#define _ICGENERATOR_H
+
+#include "expression.h"
+
+typedef enum
+{
+	S_ADD = 0, 
+	S_SUB = 1,  
+	S_MUL = 2,
+	S_DIV = 3
+} o_type;
+
+typedef enum
+{
+	SYM_OPERATOR, //+-* /
+	SYM_VAR, 
+	SYM_INT, 
+	SYM_STRING, 
+	SYM_FLOAT64
+} symbol_type;
+
+typedef struct
+{
+	char type;
+	char* name;
+} var_data_t;
+
+typedef struct 
+{
+	symbol_type sym_type;
+	void *data; //pointer to: var_type or o_type or constant
+} symbol_t;
+
+bool generate_internal_code(dll_t*);
+
+#endif
