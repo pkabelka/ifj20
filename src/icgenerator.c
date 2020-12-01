c06f3bf7f0b5d98d26ae7ebfe18c75892726dfdc
MoudryDaniel <xmoudr01@stud.fit.vutbr.cz>
2020-12-01 11:46:19 +0100

    various fixes and added concatenation of strings

--- src/icgenerator.c
+++ src/icgenerator.c
@@ -1,16 +1,20 @@
 #include "icgenerator.h"
+#include "str.c"
 
 bool generate_internal_code(dll_t *list) {
     symbol_type previous_type;
     symbol_type last_type;
+    o_type operator_type;
+    int index = 0;
 
     dll_node_t *tmp = list->first;
 
-    for (int i = 0; i < list->size; i++) {
+    while (tmp != NULL) {
         switch (((symbol_t*)tmp->data)->sym_type) {
             case SYM_OPERATOR:
                 if (last_type == previous_type) {
-                    switch(*((o_type*)((symbol_t*)tmp->data)->data)) {
+                    operator_type = *((o_type*)((symbol_t*)tmp->data)->data);
+                    switch(operator_type) {
                         case S_ADD:
                             switch (last_type) {
                                 case SYM_VAR:
@@ -28,10 +32,12 @@ bool generate_internal_code(dll_t *list) {
                                     ((symbol_t*)tmp->data)->data = float_var_data;
                                     break;
                                 case SYM_STRING: ;
-                                    /*string *string_var_data = malloc(sizeof(string));
-                                    *string_var_data = *((char*)((symbol_t*)tmp->prev->prev->data)->data) + *((char*)((symbol_t*)tmp->prev->data)->data);
+                                    string *string_var_data = malloc(sizeof(string));
+                                    str_init(string_var_data);
+                                    str_add_const(string_var_data, ((string*)((symbol_t*)tmp->prev->prev->data)->data)->str);
+                                    str_add_const(string_var_data, ((string*)((symbol_t*)tmp->prev->data)->data)->str);
                                     ((symbol_t*)tmp->data)->sym_type = SYM_STRING;
-                                    ((symbol_t*)tmp->data)->data = string_var_data;*/
+                                    ((symbol_t*)tmp->data)->data = string_var_data;
                                     break;
                                 default:
                                     break;
@@ -55,10 +61,6 @@ bool generate_internal_code(dll_t *list) {
                                     ((symbol_t*)tmp->data)->data = float_var_data;
                                     break;
                                 case SYM_STRING:
-                                    /*string *string_var_data = malloc(sizeof(string));
-                                    *string_var_data = *((char*)((symbol_t*)tmp->prev->prev->data)->data) - *((char*)((symbol_t*)tmp->prev->data)->data);
-                                    ((symbol_t*)tmp->data)->sym_type = SYM_STRING;
-                                    ((symbol_t*)tmp->data)->data = string_var_data;*/
                                     break;
                                 default:
                                     break;
@@ -113,29 +115,41 @@ bool generate_internal_code(dll_t *list) {
                             }
                             break;
                     }
+                    dll_delete(list, index - 1, free);
+                    index--;
+                    dll_delete(list, index - 1, free);
+                    index--;
                 }
-                dll_delete(list, i - 2, free);
-                dll_delete(list, i - 1, free);
+                else {
+                    previous_type = last_type;
+                    last_type = SYM_OPERATOR;
+                    index++;
+                    tmp = tmp->next;
+                }
                 break;
 
             case SYM_VAR:
                 previous_type = last_type;
                 last_type = SYM_VAR;
+                index++;
                 tmp = tmp->next;
                 break;
             case SYM_INT:
                 previous_type = last_type;
                 last_type = SYM_INT;
+                index++;
                 tmp = tmp->next;
                 break;
             case SYM_FLOAT64:
                 previous_type = last_type;
                 last_type = SYM_FLOAT64;
+                index++;
                 tmp = tmp->next;
                 break;
             case SYM_STRING:
                 previous_type = last_type;
                 last_type = SYM_STRING;
+                index++;
                 tmp = tmp->next;
                 break;
         }
